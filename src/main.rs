ed03850f94125c60d895c63fe7879e6a92f52bbe
Kamal Marhubi <kamal@marhubi.com>
2016-01-12 11:22:37 -0500

    Add example showing issue

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate clap;
+
+use clap::{App, Arg};
+use clap::AppSettings::TrailingVarArg;
+
 fn main() {
-    println!("Hello, world!");
+    let _matches = App::new("clap-optional-args-help")
+                      .setting(TrailingVarArg)
+                      .arg(Arg::with_name("COMMAND").help("Command to run").index(1))
+                      .arg(Arg::with_name("ARG")
+                               .help("Arguments for COMMAND")
+                               .multiple(true)
+                               .requires("COMMAND")
+                               .index(2))
+                      .get_matches();
 }
